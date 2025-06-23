3e4619b05e7d716137395cab6b79d403e0dbdca5
Joseph <joeavaikath@gmail.com>
2025-06-23 16:57:30 -0400

    First draft

---
+++ cmd/non-admin/nonadminbackup_builder.go
@@ -0,0 +1,164 @@
+/*
+Copyright The Velero Contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package nonadmin
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	nacv1alpha1 "github.com/migtools/oadp-non-admin/api/v1alpha1"
+)
+
+/*
+
+Example usage:
+
+var nonAdminBackup = builder.ForNonAdminBackup("user-namespace", "backup-1").
+	ObjectMeta(
+		builder.WithLabels("foo", "bar"),
+	).
+	BackupSpec(nacv1alpha1.NonAdminBackupSpec{
+		BackupSpec: &velerov1api.BackupSpec{
+			IncludedNamespaces: []string{"app-namespace"},
+		},
+	}).
+	Result()
+
+*/
+
+// NonAdminBackupBuilder builds NonAdminBackup objects.
+type NonAdminBackupBuilder struct {
+	object *nacv1alpha1.NonAdminBackup
+}
+
+// ForNonAdminBackup is the constructor for a NonAdminBackupBuilder.
+func ForNonAdminBackup(ns, name string) *NonAdminBackupBuilder {
+	return &NonAdminBackupBuilder{
+		object: &nacv1alpha1.NonAdminBackup{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: nacv1alpha1.GroupVersion.String(),
+				Kind:       "NonAdminBackup",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: ns,
+				Name:      name,
+			},
+		},
+	}
+}
+
+// Result returns the built NonAdminBackup.
+func (b *NonAdminBackupBuilder) Result() *nacv1alpha1.NonAdminBackup {
+	return b.object
+}
+
+// ObjectMeta applies functional options to the NonAdminBackup's ObjectMeta.
+func (b *NonAdminBackupBuilder) ObjectMeta(opts ...ObjectMetaOpt) *NonAdminBackupBuilder {
+	for _, opt := range opts {
+		opt(b.object)
+	}
+
+	return b
+}
+
+// BackupSpec sets the NonAdminBackup's backup spec.
+func (b *NonAdminBackupBuilder) BackupSpec(spec nacv1alpha1.NonAdminBackupSpec) *NonAdminBackupBuilder {
+	b.object.Spec = spec
+	return b
+}
+
+// Phase sets the NonAdminBackup's phase.
+func (b *NonAdminBackupBuilder) Phase(phase nacv1alpha1.NonAdminPhase) *NonAdminBackupBuilder {
+	b.object.Status.Phase = phase
+	return b
+}
+
+// VeleroBackup sets the reference to the created Velero backup.
+func (b *NonAdminBackupBuilder) VeleroBackup(backupName, backupNamespace string) *NonAdminBackupBuilder {
+	if b.object.Status.VeleroBackup == nil {
+		b.object.Status.VeleroBackup = &nacv1alpha1.VeleroBackup{}
+	}
+	b.object.Status.VeleroBackup.Name = backupName
+	b.object.Status.VeleroBackup.Namespace = backupNamespace
+	return b
+}
+
+// Conditions sets the NonAdminBackup's conditions.
+func (b *NonAdminBackupBuilder) Conditions(conditions []metav1.Condition) *NonAdminBackupBuilder {
+	b.object.Status.Conditions = conditions
+	return b
+}
+
+// WithStatus sets the NonAdminBackup's status.
+func (b *NonAdminBackupBuilder) WithStatus(status nacv1alpha1.NonAdminBackupStatus) *NonAdminBackupBuilder {
+	b.object.Status = status
+	return b
+}
+
+// ObjectMetaOpt is a functional option for setting ObjectMeta properties.
+type ObjectMetaOpt func(obj metav1.Object)
+
+// WithLabels returns a functional option that sets labels on an object.
+func WithLabels(key, value string) ObjectMetaOpt {
+	return func(obj metav1.Object) {
+		labels := obj.GetLabels()
+		if labels == nil {
+			labels = make(map[string]string)
+		}
+		labels[key] = value
+		obj.SetLabels(labels)
+	}
+}
+
+// WithLabelsMap returns a functional option that sets labels from a map on an object.
+func WithLabelsMap(labels map[string]string) ObjectMetaOpt {
+	return func(obj metav1.Object) {
+		existingLabels := obj.GetLabels()
+		if existingLabels == nil {
+			existingLabels = make(map[string]string)
+		}
+		for k, v := range labels {
+			existingLabels[k] = v
+		}
+		obj.SetLabels(existingLabels)
+	}
+}
+
+// WithAnnotations returns a functional option that sets annotations on an object.
+func WithAnnotations(key, value string) ObjectMetaOpt {
+	return func(obj metav1.Object) {
+		annotations := obj.GetAnnotations()
+		if annotations == nil {
+			annotations = make(map[string]string)
+		}
+		annotations[key] = value
+		obj.SetAnnotations(annotations)
+	}
+}
+
+// WithAnnotationsMap returns a functional option that sets annotations from a map on an object.
+func WithAnnotationsMap(annotations map[string]string) ObjectMetaOpt {
+	return func(obj metav1.Object) {
+		existingAnnotations := obj.GetAnnotations()
+		if existingAnnotations == nil {
+			existingAnnotations = make(map[string]string)
+		}
+		for k, v := range annotations {
+			existingAnnotations[k] = v
+		}
+		obj.SetAnnotations(existingAnnotations)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,37 @@ module github.com/joeavaikath/sample-plugin
 go 1.24.3
 
 require (
+	github.com/migtools/oadp-non-admin v0.0.0-20250505165924-a9be4321819c
 	github.com/spf13/cobra v1.9.1
+	github.com/spf13/pflag v1.0.6
 	github.com/vmware-tanzu/velero v1.16.1
+	k8s.io/apimachinery v0.33.1
+	k8s.io/client-go v0.33.1
+	sigs.k8s.io/controller-runtime v0.19.3
 )
 
 require (
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/aws/aws-sdk-go v1.44.253 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.30.3 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.5.4 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.26.3 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.26 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.11 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.15.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.2.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.2.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.16.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.48.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.22.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.26.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.30.3 // indirect
+	github.com/aws/smithy-go v1.20.3 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
@@ -26,6 +51,7 @@ require (
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
@@ -38,13 +64,13 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/openshift/oadp-operator v1.0.2-0.20250425163444-a21288a0f20b // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.20.5 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
 	golang.org/x/mod v0.22.0 // indirect
@@ -61,13 +87,10 @@ require (
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/api v0.33.1 // indirect
 	k8s.io/apiextensions-apiserver v0.31.3 // indirect
-	k8s.io/apimachinery v0.33.1 // indirect
 	k8s.io/cli-runtime v0.31.3 // indirect
-	k8s.io/client-go v0.33.1 // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20250318190949-c8a335a9a2ff // indirect
 	k8s.io/utils v0.0.0-20241104100929-3ea5e8cea738 // indirect
-	sigs.k8s.io/controller-runtime v0.19.3 // indirect
 	sigs.k8s.io/json v0.0.0-20241010143419-9aa6b5e7a4b3 // indirect
 	sigs.k8s.io/randfill v1.0.0 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.6.0 // indirect

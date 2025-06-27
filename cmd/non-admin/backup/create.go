44104bbbe6cdd4c1232cdc55bbbe4d1c9fb38c31
Joseph <joeavaikath@gmail.com>
2025-06-27 11:15:32 -0400

    Restructure non-admin dir

--- cmd/non-admin/backup/create.go
+++ cmd/non-admin/backup/create.go
@@ -1,4 +1,4 @@
-package nonadmin
+package backup
 
 /*
 Copyright The Velero Contributors.
--- cmd/non-admin/nonadmin.go
+++ cmd/non-admin/nonadmin.go
@@ -1,6 +1,7 @@
 package nonadmin
 
 import (
+	"github.com/migtools/oadp-cli/cmd/non-admin/backup"
 	"github.com/spf13/cobra"
 	"github.com/vmware-tanzu/velero/pkg/client"
 )
@@ -14,7 +15,15 @@ func NewNonAdminCommand(f client.Factory) *cobra.Command {
 	}
 
 	// Add backup subcommand
-	c.AddCommand(NewBackupCommand(f))
+	c.AddCommand(backup.NewBackupCommand(f))
 
 	return c
+}
+
+// NewNonAdminFactory creates a client factory for NonAdminBackup operations
+// that uses the current kubeconfig context namespace instead of hardcoded openshift-adp
+func NewNonAdminFactory() client.Factory {
+	// Don't set a default namespace, let it use the kubeconfig context
+	cfg := client.VeleroConfig{}
+	return client.NewFactory("oadp-nonadmin-cli", cfg)
 }
